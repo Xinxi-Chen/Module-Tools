52a107fc27b5c4ba85196e593c8c77c437610c3a
Xinxi <xinxichen1022@gmail.com>
2026-03-21 14:35:44 +0100

    cat completed

--- individual-shell-tools/cat/script-03.sh
+++ individual-shell-tools/cat/script-03.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 # TODO: Write a command to output the contents of the file `helper-3.txt` inside the helper-files directory to the terminal.
 # This time, we also want to see the line numbers in the output.
-#
+cat -n helper-files/helper-3.txt 
 # The output of this command should be something like:
 # 1 It looked delicious.
 # 2 I was tempted to take a bite of it.
