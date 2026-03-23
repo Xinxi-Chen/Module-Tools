5a478149455830211121455d2f0e4e40165ed6c0
Xinxi <xinxichen1022@gmail.com>
2026-03-23 11:05:48 +0100

    grep completed

--- individual-shell-tools/grep/script-03.sh
+++ individual-shell-tools/grep/script-03.sh
@@ -3,4 +3,5 @@
 set -euo pipefail
 
 # TODO: Write a command to output the number of lines in dialogue.txt that contain the word Doctor (regardless of case).
+grep -ci "Doctor" dialogue.txt
 # The output should be exactly the number 9.
